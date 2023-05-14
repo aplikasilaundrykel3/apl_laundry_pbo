75a5267a599d21ea823e5b42d68832707aee3e1c
aplikasilaundrykel3 <132528335+aplikasilaundrykel3@users.noreply.github.com>
2023-05-14 19:27:03 +0700

    Add files via upload

---
+++ Apl_laundry.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apl_laundry;
+
+/**
+ *
+ * @author RPL
+ */
+public class Apl_laundry {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
